737cc8a327f6281e126a3f2856c9eb11aad0c724
macszl <235989@edu.p.lodz.pl>
2021-02-25 16:48:51 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/2502_GITHUB_ZAL.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2502_GITHUB_ZAL.exe"
+  "2502_GITHUB_ZAL.exe.manifest"
+  "2502_GITHUB_ZAL.pdb"
+  "CMakeFiles/2502_GITHUB_ZAL.dir/main.c.obj"
+  "lib2502_GITHUB_ZAL.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/2502_GITHUB_ZAL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
